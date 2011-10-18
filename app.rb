3ef71165ef9087e27db6857cdfeb711bceaa6056
Ju Liu <ju.liu@welaika.com>
2011-10-19 00:03:25 +0200

    some refactoring

--- app.rb
+++ app.rb
@@ -2,11 +2,20 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'alphadecimal'
 
 class ShortenedUrl < ActiveRecord::Base
   validates_uniqueness_of :url
   validates_presence_of :url
   validates_format_of :url, :with => /^\b((?:https?:\/\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))$/
+
+  def shorten
+    self.id.alphadecimal
+  end
+
+  def self.find_by_shortened(shortened)
+    find(shortened.alphadecimal)
+  end
 end
 
 get '/' do
@@ -15,14 +24,10 @@ end
 
 post '/' do
   @short_url = ShortenedUrl.find_or_create_by_url(params[:url])
-  if @short_url.valid?
-    haml :success
-  else
-    haml :index
-  end
+  haml :index
 end
 
 get '/:shortened' do
-  short_url = ShortenedUrl.find(params[:shortened])
+  short_url = ShortenedUrl.find_by_shortened(params[:shortened])
   redirect short_url.url
 end
